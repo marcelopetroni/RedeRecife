a16d2e3ff48112ef37772f2d50d25fa6d368d874
marcelopetroni <marceloapetroni@hotmail.com>
2024-07-05 03:22:27 -0300

    testes

--- src/telas/Rotas.js
+++ src/telas/Rotas.js
@@ -1,47 +1,124 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image, Text, StyleSheet } from 'react-native';
+import Newspaper from '../imagens/Navbar/newspaperSvg.svg'
+import Plus  from '../imagens/Navbar/plusSvg.svg'
+import Alerta  from '../imagens/Navbar/alertaSvg.svg'
 
 import TelaHome from './TelaHome';
 import TelaEducaHub from './TelaEducaHub';
 import TelaAjuda from './TelaAjuda';
 import TelaAlerta from './TelaAlerta';
-import TelaLogin from './TelaLogin';
-import TelaCadastro from './TelaCadastro'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+  title: {
+    color: "rgba(77, 77, 77, 0.46)",
+    fontSize: 13,
+    fontWeight: "500"
+  },
+});
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator 
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: [
+          {
+            height: 100,
+            display: 'flex',
+            alignItems: 'center',
+          },
+        ],
+      }}
+    >
+      <Tab.Screen
+        name="TelaEducaHub"
+        component={TelaEducaHub}
+        options={{
+          title: '',
+          headerTransparent: true,
+          tabBarIcon: () => (
+            <>
+              <Newspaper width={40} height={40} />
+              <Text style={styles.title}>EducaHub</Text>
+            </>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TelaAjuda"
+        component={TelaAjuda}
+        options={{
+          title: '',
+          headerTransparent: true,
+          tabBarIcon: () => (
+            <>
+              <Plus width={40} height={40} />
+              <Text style={styles.title}>AjudaRecife</Text>
+            </>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TelaAlerta"
+        component={TelaAlerta}
+        options={{
+          title: '',
+          headerTransparent: true,
+          tabBarIcon: () => (
+            <>
+              <Alerta width={40} height={40} />
+              <Text style={styles.title}>Alerta</Text>
+            </>
+          ),
+        }}
+      />
+      <Stack.Screen
+          name="TelaHome"
+          component={TelaHome}
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+            tabBarButton: () => null, 
+          }}
+        />
+    </Tab.Navigator>
+  );
+};
 function Rotas() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="TelaHome">
-      <Stack.Screen name="TelaLogin" component={TelaLogin} 
-            options={{
-              title: '',
-              headerTransparent: true,
-              headerShown: false,
-            }}
-          />
-        <Stack.Screen name="TelaCadastro" component={TelaCadastro} 
+      <Stack.Navigator initialRouteName="Main">
+      <Stack.Screen
+          name="Main"
+          component={TabNavigator}
           options={{
             title: '',
             headerTransparent: true,
             headerShown: false,
           }}
         />
+      <Stack.Screen name="TelaHome" 
+        component={TelaHome} 
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+          }}
+          />
         <Stack.Screen name="TelaEducaHub" component={TelaEducaHub} 
             options={{
               title: '',
               headerTransparent: true,
               headerShown: false,
             }}
-          />
-        <Stack.Screen name="TelaHome" component={TelaHome} 
-          options={{
-            title: '',
-            headerTransparent: true,
-            headerShown: false,
-          }}
           />
         <Stack.Screen name="TelaAjuda" component={TelaAjuda} 
           options={{
